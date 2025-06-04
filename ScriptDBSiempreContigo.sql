bd8ed1870f841dd5d243ab35714a1e598b2b924a
Kautto06 <gerardobenacaut@gmail.com>
2025-06-04 12:13:23 -0400

    Changes to the table usuarios in the script

--- ScriptDBSiempreContigo.sql
+++ ScriptDBSiempreContigo.sql
@@ -3,6 +3,8 @@ CREATE TABLE usuarios (
   nombre VARCHAR(100) NOT NULL,
   correo VARCHAR(100) UNIQUE NOT NULL,
   contrasena VARCHAR(255) NOT NULL,
+  telefono VARCHAR(20),
+  direccion VARCHAR(255),
   rol ENUM('Admin', 'Cuidador', 'Familiar'),
   activo BOOLEAN DEFAULT TRUE,
   conectado BOOLEAN DEFAULT FALSE
